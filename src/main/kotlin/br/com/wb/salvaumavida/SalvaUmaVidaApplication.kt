8c363ac15b4890bce355ac6bf1eeab682b08fa46
viniciosbastos <aurelio.vinicios1996@gmail.com>
2020-07-21 19:37:09 -0300

    First commit

---
+++ src/main/kotlin/br/com/wb/salvaumavida/SalvaUmaVidaApplication.kt
@@ -0,0 +1,11 @@
+package br.com.wb.salvaumavida
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SalvaUmaVidaApplication
+
+fun main(args: Array<String>) {
+	runApplication<SalvaUmaVidaApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/wb/salvaumavida/SalvaUmaVidaApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.wb.salvaumavida
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SalvaUmaVidaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
