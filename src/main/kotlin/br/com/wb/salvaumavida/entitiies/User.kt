0cac239bd28fcee49d61411211a897eb50a73522
viniciosbastos <aurelio.vinicios1996@gmail.com>
2020-07-25 20:36:58 -0300

    Minor adjustments to user entity

--- src/main/kotlin/br/com/wb/salvaumavida/entitiies/User.kt
+++ src/main/kotlin/br/com/wb/salvaumavida/entitiies/User.kt
@@ -1,6 +1,5 @@
 package br.com.wb.salvaumavida.entitiies
 
-import com.fasterxml.jackson.annotation.JsonIgnore
 import javax.persistence.*
 
 @Entity
@@ -11,15 +10,18 @@ data class User (
         var name: String,
         var email: String,
         var password: String,
-        var detail: String,
-        var address: String,
-        var addressLatitude: Double,
-        var addressLongitude: Double,
+        var detail: String?,
+        var address: String?,
+        var addressLatitude: Double?,
+        var addressLongitude: Double?,
         @Enumerated(EnumType.STRING)
         var type: UserType,
 
         @OneToMany(mappedBy = "user", cascade = [CascadeType.ALL])
-        var campaigns: List<Campaign>?
+        var campaigns: List<Campaign>?,
+
+        @OneToMany(mappedBy = "user", cascade = [CascadeType.ALL])
+        var events: List<Event>?
 
 ){
 
